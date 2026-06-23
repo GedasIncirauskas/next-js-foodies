16b645c3fa5b94c01bd15f713ac9115328263705
Gedas.Incirauskas <gedas.incirauskas@luminorgroup.com>
2026-06-23 10:39:26 +0300

    added meals components and style

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import MealsGrid from "../components/meals/meals-grid";
+import styles from "./page.module.css";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Meals Page for <span className={styles.highlight}>you</span>
+        </h1>
+        <p>Discover and share your favorite meals with our community!</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share your meal</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
